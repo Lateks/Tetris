1ba0fc1169d76b89f02d4d6562210c55097428be
Laura Leppänen <laura.leppanen@iki.fi>
2012-01-16 19:37:16 +0200

    All tests pass.

--- tests/rotating_pieces_of_blocks_test.py
+++ tests/rotating_pieces_of_blocks_test.py
@@ -7,13 +7,14 @@ class RotatingPiecesOfBlocksTestSuiteGenerator:
 
     def suite(self):
         piece3x3 = self.a_piece_of_3x3_blocks_suite()
-        return piece3x3
+        piece5x5 = self.a_piece_of_5x5_blocks_suite()
+        return unittest.TestSuite([piece3x3, piece5x5])
 
     def a_piece_of_3x3_blocks_suite(self):
         return self.loader.loadTestsFromTestCase(APieceOf3x3Blocks)
 
-    #def a_piece_of_5x5_blocks_suite(self):
-    #    return self.loader.loadTestsFromTestCase(APieceOf5x5Blocks)
+    def a_piece_of_5x5_blocks_suite(self):
+        return self.loader.loadTestsFromTestCase(APieceOf5x5Blocks)
 
 class APieceOf3x3Blocks(unittest.TestCase):
     def setUp(self):
@@ -39,32 +40,32 @@ class APieceOf3x3Blocks(unittest.TestCase):
                          "...\n"
         self.assertEqual(expected_piece, str(piece))
 
-#class APieceOf3x3Blocks(unittest.TestCase):
-#    def setUp(self):
-#        self.piece_repr = "..XXX\n" +
-#                          "..XX.\n" +
-#                          "..X..\n" +
-#                          ".....\n" +
-#                          ".....\n"
-#        self.piece = Piece(piece_repr)
-#
-#    def test_consists_of_many_blocks(self):
-#        assertEqual(self.piece_repr, str(self.piece))
-#
-#    def test_can_be_rotated_right(self):
-#        self.piece = self.piece.rotate_right()
-#        expected_piece = ".....\n" +
-#                         ".....\n" +
-#                         "..XXX\n" +
-#                         "...XX\n" +
-#                         "....X\n"
-#        assertEqual(expected_piece, str(self.piece))
-#
-#    def test_can_be_rotated_left(self):
-#        self.piece = self.piece.rotate_left()
-#        expected_piece = "X....\n" +
-#                         "XX...\n" +
-#                         "XXX..\n" +
-#                         ".....\n" +
-#                         ".....\n"
-#        assertEqual(expected_piece, str(self.piece))
+class APieceOf5x5Blocks(unittest.TestCase):
+    def setUp(self):
+        self.piece_repr = "..XXX\n" + \
+                          "..XX.\n" + \
+                          "..X..\n" + \
+                          ".....\n" + \
+                          ".....\n"
+        self.piece = Piece(self.piece_repr)
+
+    def test_consists_of_many_blocks(self):
+        self.assertEqual(self.piece_repr, str(self.piece))
+
+    def test_can_be_rotated_right(self):
+        piece = self.piece.rotate_right()
+        expected_piece = ".....\n" + \
+                         ".....\n" + \
+                         "..XXX\n" + \
+                         "...XX\n" + \
+                         "....X\n"
+        self.assertEqual(expected_piece, str(piece))
+
+    def test_can_be_rotated_left(self):
+        piece = self.piece.rotate_left()
+        expected_piece = "X....\n" + \
+                         "XX...\n" + \
+                         "XXX..\n" + \
+                         ".....\n" + \
+                         ".....\n"
+        self.assertEqual(expected_piece, str(piece))
