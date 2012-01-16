0f96adf10c0c5610ef67dc558bf362a4fb47c5b9
Laura Leppänen <laura.leppanen@iki.fi>
2012-01-16 19:32:41 +0200

    Refactor rotations.

--- src/piece.py
+++ src/piece.py
@@ -7,26 +7,40 @@ class Piece:
         return self.__concatenate_rows_to_string(self.piece)
 
     def rotate_right(self):
-        rows = cols = len(self.piece)
-        rotated_piece = list()
-        for col in range(0, cols):
-            new_row = ''
-            for row in reversed(range(0, rows)):
-                new_row += self.piece[row][col]
-            rotated_piece.append(new_row)
+        ordered_rows, ordered_cols = self.__right_rotation_params()
+        rotated_piece = self.__rotate_piece(ordered_rows, ordered_cols)
         rotated_piece_repr = self.__concatenate_rows_to_string(rotated_piece)
         return Piece(rotated_piece_repr)
 
     def rotate_left(self):
-        rows = cols = len(self.piece)
-        rotated_piece = list()
-        for col in reversed(range(0, cols)):
-            new_row = ''
-            for row in range(0, cols):
-                new_row += self.piece[row][col]
-            rotated_piece.append(new_row)
+        ordered_rows, ordered_cols = self.__left_rotation_params()
+        rotated_piece = self.__rotate_piece(ordered_rows, ordered_cols)
         rotated_piece_repr = self.__concatenate_rows_to_string(rotated_piece)
         return Piece(rotated_piece_repr)
+
+    def __right_rotation_params(self):
+        rows = cols = len(self.piece)
+        ordered_rows = self.__true_reverse_list(range(0, rows))
+        ordered_cols = range(0, cols)
+        return ordered_rows, ordered_cols
+
+    def __left_rotation_params(self):
+        rows = cols = len(self.piece)
+        ordered_rows = range(0, rows)
+        ordered_cols = self.__true_reverse_list(range(0, cols))
+        return ordered_rows, ordered_cols
+
+    def __true_reverse_list(self, list_to_reverse):
+        return list(reversed(list_to_reverse))
+
+    def __rotate_piece(self, ordered_rows, ordered_columns):
+        rotated_piece = list()
+        for col in ordered_columns:
+            new_row = ''
+            for row in ordered_rows:
+                new_row += self.piece[row][col]
+            rotated_piece.append(new_row)
+        return rotated_piece
 
     def __concatenate_rows_to_string(self, rows):
         representation = ''
