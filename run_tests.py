1fa24fa355a3d54cb528864ccebd2b7f764e9ade
Laura Leppänen <laura.leppanen@iki.fi>
2012-01-19 14:20:44 +0200

    Add tests for falling pieces.

--- run_tests.py
+++ run_tests.py
@@ -2,13 +2,17 @@ import unittest
 from tests.falling_blocks_test import FallingBlockTestSuiteGenerator
 from tests.rotating_pieces_of_blocks_test import RotatingPiecesOfBlocksTestSuiteGenerator
 from tests.rotating_tetrominoes_test import RotatingTetrominoesTestSuiteGenerator
+from tests.falling_pieces_test import FallingPiecesTestSuiteGenerator
 
 def run_tests():
     falling_blocks_suite = get_falling_blocks_suite()
     rotating_pieces_suite = get_rotating_pieces_suite()
     rotating_tetrominoes_suite = get_rotating_tetrominoes_suite()
+    falling_pieces_suite = get_falling_pieces_suite()
+
     test_suite = unittest.TestSuite([falling_blocks_suite,
-        rotating_pieces_suite, rotating_tetrominoes_suite])
+        rotating_pieces_suite, rotating_tetrominoes_suite,
+        falling_pieces_suite])
     unittest.TextTestRunner(verbosity = 2).run(test_suite)
 
 def get_falling_blocks_suite():
@@ -21,6 +25,10 @@ def get_rotating_pieces_suite():
 
 def get_rotating_tetrominoes_suite():
     test_suite_gen = RotatingTetrominoesTestSuiteGenerator()
+    return test_suite_gen.suite()
+
+def get_falling_pieces_suite():
+    test_suite_gen = FallingPiecesTestSuiteGenerator()
     return test_suite_gen.suite()
 
 if __name__ == '__main__':
---
+++ tests/falling_pieces_test.py
@@ -0,0 +1,91 @@
+import unittest
+import src.tetrominoe as tetrominoe
+from src.board import Board
+
+class FallingPiecesTestSuiteGenerator:
+    def __init__(self):
+        self.loader = unittest.TestLoader()
+
+    def suite(self):
+        piece_dropped = self.when_a_piece_is_dropped_suite()
+        return piece_dropped
+
+    def when_a_piece_is_dropped_suite(self):
+        return self.loader.loadTestsFromTestCase(WhenAPieceIsDropped)
+
+class WhenAPieceIsDropped(unittest.TestCase):
+    def setUp(self):
+        self.board = Board(6, 8)
+
+#    def test_t_shape_starts_from_top_middle(self):
+#        self.board.drop(tetrominoe.T_SHAPE)
+#        self.assertEqual("....T...\n" + \
+#                         "...TTT..\n" + \
+#                         "........\n" + \
+#                         "........\n" + \
+#                         "........\n" + \
+#                         "........\n", str(self.board))
+#
+#    def test_o_shape_starts_from_top_middle(self):
+#        self.board.drop(tetrominoe.O_SHAPE)
+#        self.assertEqual("...OO...\n" + \
+#                         "...OO...\n" + \
+#                         "........\n" + \
+#                         "........\n" + \
+#                         "........\n" + \
+#                         "........\n", str(self.board))
+
+#class WhenAPieceReachesTheBottom(unittest.TestCase):
+#    def setUp(self):
+#        self.board = Board(6, 8)
+#        self.board.drop(tetrominoe.T_SHAPE)
+#        for i in range(0, 4)
+#        self.board.tick()
+#
+#    def test_piece_is_still_falling_on_the_last_row(self):
+#        self.assertTrue(self.board.has_falling_blocks())
+#        self.assertEqual("........\n" + \
+#                         "........\n" + \
+#                         "........\n" + \
+#                         "........\n" + \
+#                         "....T...\n" + \
+#                         "...TTT..\n", str(self.board))
+#
+#    def test_piece_stops_when_it_hits_the_bottom(self):
+#        self.board.tick()
+#        self.assertFalse(self.board.has_falling_blocks())
+#        self.assertEqual("........\n" + \
+#                         "........\n" + \
+#                         "........\n" + \
+#                         "........\n" + \
+#                         "....T...\n" + \
+#                         "...TTT..\n", str(self.board))
+#
+#class WhenAPieceLandsOnAnotherPiece(unittest.TestCase):
+#    def setUp(self):
+#        self.board = Board(6, 8)
+#        self.board.drop(tetrominoe.T_SHAPE)
+#        for i in range(0, 5):
+#            self.board.tick()
+#        self.board.drop(tetrominoe.T_SHAPE)
+#        self.board.tick()
+#        self.board.tick()
+#
+#    def test_piece_is_still_falling_right_above_the_other_piece(self):
+#        self.assertTrue(self.board.has_falling_blocks())
+#        self.assertEqual("........\n" + \
+#                         "........\n" + \
+#                         "....T...\n" + \
+#                         "...TTT..\n" + \
+#                         "....T...\n" + \
+#                         "...TTT..\n", str(self.board))
+#
+#    def test_piece_stops_when_it_hits_the_other_piece(self):
+#        self.board.tick()
+#        self.assertFalse(self.board.has_falling_blocks())
+#        self.assertEqual("........\n" + \
+#                         "........\n" + \
+#                         "....T...\n" + \
+#                         "...TTT..\n" + \
+#                         "....T...\n" + \
+#                         "...TTT..\n", str(self.board))
